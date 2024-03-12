4ac4e2dfefef36ec1045b6a33c4d8e356a5f45c2
kiore paul <kiorepaul@gmail.com>
2024-03-12 11:05:47 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -25,6 +25,7 @@ print(f"Time zone: {settings.TIME_ZONE}")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
+    path("api/v1/", include("blog.api_urls")),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
     path("accounts/", include("allauth.urls")),
